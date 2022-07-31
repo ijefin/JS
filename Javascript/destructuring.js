33d7bcf9a96cc44a9daf3fce704ba7d933318716
ijefin <jeffersonmecmiler@hotmail.com>
2022-07-31 01:53:18 -0300

    destruturação de array

--- Javascript/destructuring.js
+++ Javascript/destructuring.js
@@ -9,3 +9,24 @@ const pessoa = {
     complemento: "casa A",
   },
 };
+
+//é possível desestruturar um objeto com a nova sintaxa do ecs6 da seguinte forma
+const { nome, idade } = pessoa;
+
+//onde é declarado um bloco de código com as chaves requisitadas na sua necessidade, e atribuindo o valor ao próprio objeto
+console.log(nome, idade);
+
+//também é possível atribuir variáveis a chave do objeto para facilitar a escrita
+const { nome: n, idade: i } = pessoa;
+
+console.log(n, i);
+
+//podemos também iniciar chaves que não existem no objeto com um valor implicito
+const { cpf: c, bemHumorada = true } = pessoa;
+console.log(c, bemHumorada);
+
+//não podemos declarar uma chave inexistente no objeto
+const { conta: { agencia, saldo },
+} = pessoa;
+
+console.log(agencia, saldo);
