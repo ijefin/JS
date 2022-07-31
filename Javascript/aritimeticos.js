079307b8c11b30625790ce4d6f54090a707e470e
ijefin <jeffersonmecmiler@hotmail.com>
2022-07-31 14:41:53 -0300

    relação de dados

---
+++ Javascript/aritimeticos.js
@@ -0,0 +1,10 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * d;
+const divisao = d / a;
+const modulo = a % 2;
+
+console.log(`Soma: ${soma}, Subtração: ${subtracao}, Multiplicação: ${multiplicacao}, Divisão: ${divisao} ,Modulo: ${modulo} `);
+|
