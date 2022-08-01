b8a9615e3b2bb881e68e2676d9d25caa8ae3fc11
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-01 15:19:58 -0300

    bora

--- Javascript/forIn.js
+++ Javascript/forIn.js
@@ -1,6 +1,19 @@
 const numeros = [1, 2, 3, 5, 7, 8, 9, 4, 12, 5, 6, 7];
 
 //trabalha com o índice e não com o valor
-for(let i in numeros){
-    console.log(`${i}: ${numeros[i]}`)
+for (let i in numeros) {
+  console.log(`${i}: ${numeros[i]}`);
+}
+
+const eu = {
+  nome: "Jefferson",
+  idade: 23,
+  cpf: "vish",
+  fodao: "Demais",
+  namora: "Uma gostosa",
+  transaGostoso: "Pra um krl bixo slc",
+};
+
+for (let attr in eu) {
+  console.log(`${attr}: ${eu[attr]}`);
 }
