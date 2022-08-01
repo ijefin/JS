c566c91897c919d6257efa74090d061611d7628f
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-01 14:56:17 -0300

    while

--- Javascript/while.js
+++ Javascript/while.js
@@ -1,13 +1,20 @@
 function getInteiroAleatorioEntre(min, max) {
-    const valor = Math.random() * (max - min) + min
-    return Math.floor(valor)
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
 }
- 
-let opcao //somente iniciada a variável, sem valor inicial!
- 
+
+let opcao; //somente iniciada a variável, sem valor inicial!
+
 do {
-    opcao = getInteiroAleatorioEntre(-1, 10)
-    console.log(`Opção escolhida foi ${opcao}.`)
-} while (opcao != -1)
- 
-console.log('Até a próxima!')
+  opcao = getInteiroAleatorioEntre(-1, 10);
+  console.log(`Opção escolhida foi ${opcao}.`);
+} while (opcao != -1);
+
+console.log("Até a próxima!");
+
+let contador = 0;
+
+while (contador <= 10) {
+  console.log(`contando até: ${contador}`);
+  contador++
+}
