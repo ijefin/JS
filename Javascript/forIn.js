8d46986b8d144f5196568fde651db3f91f7f85d5
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-01 15:35:12 -0300

    forIn

--- Javascript/forIn.js
+++ Javascript/forIn.js
@@ -19,3 +19,29 @@ for (let attr in eu) {
 }
 
 //utilizar para percorrer objetos buscando a chave (atributo)
+
+//BREAK: pós satisfazer a condição, pausa o laço e vai para o proximo bloco de código (desvio de fluxo)
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let c in nums) {
+  console.log(`${c}: ${nums[c]}`);
+  if (c == 5) {
+    break;
+  }
+}
+
+//pós satisfazer a condição, continua a repetição do laço
+for (let x in nums) {
+  console.log(`${x}: ${nums[x]}`);
+  if (x == 5) {
+    continue;
+  }
+}
+
+//caso eu deseje interromper a operação do laço mais distante, utilizo um rótulo, pois o break irá parar o laço for mais próximo
+externo: for (let a in nums) {
+  for (let b in nums) {
+    if (a == 2 && b == 3) break externo;
+    console.log(`Par = ${a}, ${b}`);
+  }
+}
