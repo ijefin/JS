a2592a378438aa3d4f9658856efc027b7c3770b9
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-02 17:13:12 -0300

    default values to a function param

--- Javascript/parametroPadrao.js
+++ Javascript/parametroPadrao.js
@@ -16,4 +16,12 @@ function soma2(a, b, c) {
   b = 1 in arguments ? b : 1;
   // terceira forma
   c = isNaN(c) ? 1 : c;
+  return a + b + c;
 }
+
+console.log(soma2(1), soma2(2, 5), soma2(0, 0, 0));
+
+//melhor forma de fazer =)
+
+//defina o valor padrão logo pós o parâmetro
+const soma = (a = 1, b = 2, c = 3) => a + b + c;
