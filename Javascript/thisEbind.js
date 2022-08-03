53b58b2c93f8971f85e07d60beefd025229cd141
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-03 19:25:33 -0300

    this

---
+++ Javascript/thisEbind.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  saudacao: "Bom Dia!",
+  falar() {
+    console.log(this.saudacao);
+  },
+};
+
+pessoa.falar();
+const falar = pessoa.falar
+falar() //conflito entre paradigmas: funcional e OO
