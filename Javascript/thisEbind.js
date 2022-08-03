61c7dd22890686d9e19ed6b1b64c3d8717405929
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-03 19:30:58 -0300

    this

--- Javascript/thisEbind.js
+++ Javascript/thisEbind.js
@@ -6,5 +6,13 @@ const pessoa = {
 };
 
 pessoa.falar();
-const falar = pessoa.falar
-falar() //conflito entre paradigmas: funcional e OO
+// const falar = pessoa.falar;
+// falar(); //conflito entre paradigmas: funcional e OO
+
+//para resolver, podemos fazer assim:
+
+//o bind consegue referênciar o this do objeto, utilizando o próprio objeto como parâmetro
+const falarDePessoa = pessoa.falar.bind(pessoa);
+falarDePessoa()
+
+bind() // método responsável para amarrar um objeto, referênciando o this do objeto.
