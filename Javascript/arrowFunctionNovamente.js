c42f4cfe9b2f2bd06f0a30fede2f0a2d7c6c3b0c
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-04 12:58:55 -0300

    revisando arrow functions

---
+++ Javascript/arrowFunctionNovamente.js
@@ -0,0 +1,17 @@
+let dobro = function (a) {
+  return 2 * a;
+};
+
+dobro = (a) => {
+  return 2 * a;
+};
+
+dobro = (a) => 2 * a;
+
+let ola = function () {
+  return "Olá";
+};
+
+ola = () => "Olá";
+
+ola = _ => "Olá";
