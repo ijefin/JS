8f7510eae3c698fcac5872285644e3757dc365ef
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-05 21:30:48 -0300

    lexical context

--- Javascript/lexicalContext.js
+++ Javascript/lexicalContext.js
@@ -11,5 +11,8 @@ function exec() {
 }
 exec();
 
-/*Por causa do contexto léxico ( contexto no qual a função foi escrita), a função encontrará o valor global,
-a informação ja definida na função é mantida até o final por conta do contexto léxico*/
+/*
+Por causa do contexto léxico ( contexto no qual a função foi escrita), a função encontrará o valor global,
+a informação ja definida na função é mantida até o final por conta do contexto léxico
+O dado será sempre buscado no escopo no qual a função foi definida
+*/
