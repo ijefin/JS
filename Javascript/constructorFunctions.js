10393e30d5af5df14d54b63296733bd83f40e52f
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-06 22:51:32 -0300

    constructor function

--- Javascript/constructorFunctions.js
+++ Javascript/constructorFunctions.js
@@ -1,27 +1,34 @@
-class Carro {
-  constructor(velocidadeMaxima = 200, delta = 5) {
-    //atributo privado
-    let velocidadeAtual = 0;
+function Carro(velocidadeMaxima = 200, delta = 5) {
+  //atributo privado
+  let velocidadeAtual = 0;
+  //declaração de variável com let e const deixa o atributo privado
 
-    //método publico
-    this.acelerar = () => {
-      velocidadeAtual + delta <= velocidadeMaxima
-        ? (velocidadeAtual += delta)
-        : (velocidadeMaxima = velocidadeAtual);
-    };
+  //metodo publico
+  this.acelerar = function () {
+    if (velocidadeAtual + delta <= velocidadeMaxima) {
+      velocidadeAtual += delta;
+    } else {
+      velocidadeAtual = velocidadeMaxima;
+    }
+  };
+  //com o this, deixa o método público
 
-    this.getVelocidadeAtual = () => velocidadeAtual;
-  }
+  //metodo publico
+
+  this.getVelocidadeAtual = function () {
+    return velocidadeAtual;
+  };
 }
 
-const uno = new Carro(150, 2);
+const uno = new Carro();
+uno.acelerar();
 uno.acelerar();
 uno.acelerar();
 uno.acelerar();
 uno.acelerar();
 console.log(uno.getVelocidadeAtual());
 
-const ferrari = new Carro(369, 86);
+const ferrari = new Carro(346, 50);
 ferrari.acelerar();
 ferrari.acelerar();
 ferrari.acelerar();
@@ -30,8 +37,3 @@ ferrari.acelerar();
 ferrari.acelerar();
 ferrari.acelerar();
 console.log(ferrari.getVelocidadeAtual());
-
-console.log(typeof Carro);
-console.log(typeof ferrari);
-
-//a função construtora é o meu molde, ( funciona como uma classe em outras langs) e meus métodos são funções.
