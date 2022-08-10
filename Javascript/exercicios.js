531056718e930b3e62755ff4e1aafcbbbb169835
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-10 17:23:28 -0300

    baskara

--- Javascript/exercicios.js
+++ Javascript/exercicios.js
@@ -85,7 +85,7 @@ function jurosSimples(capitalInicial, taxaDeJuros, tempoDeAplicacao) {
   console.log(`${jurosSimples}`);
 }
 
-jurosSimples(1000, 0.1, 0.5)
+jurosSimples(1000, 0.1, 0.5);
 
 function jurosComposto(
   capitalInicial = 0,
@@ -95,3 +95,27 @@ function jurosComposto(
   const jurosComposto = capitalInicial * (1 + taxaDeJuros) ** tempoDeAplicacao;
   console.log(jurosComposto);
 }
+
+/*Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto,
+elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três
+parâmetros, “ax2”, “bx” e “c”, de t cada possível resultadal modo que na equação: 3x² - 5x + 12 os valores seriam respectivamente: 3,
+-5, 12. Como retorno deve ser passado um vetor que tem 2 valores um parao, mesmo
+que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase:
+“Delta é negativo”.*/
+
+function baskhara(a, b, c) {
+  const resultado = [];
+  let delta = b ** 2 - 4 * a * c;
+  if (delta > 0) {
+    let x1 = ((-b + Math.sqrt(delta)) / 2) * a;
+    let x2 = ((-b - Math.sqrt(delta)) / 2) * a;
+    resultado.push(x1, x2);
+    //forma de desestruturar um ARRAY
+    // const [valor1, valor2] = [x1, x2];
+    // console.log(`x1 = ${valor1} e x2 = ${valor2}`);
+    console.log(resultado);
+  } else {
+    console.log("Ø");
+  }
+}
+baskhara(3, -15, 12);
