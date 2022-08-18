619b0bb81f390461222cabca46b090ebff16e81e
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-18 07:23:19 -0300

    exercicios

--- Javascript/exercicios.js
+++ Javascript/exercicios.js
@@ -316,6 +316,32 @@ function retornaVeiculo(veiculos) {
 }
 
 retornaVeiculo("cb 650f");
-// function converteParaLowerCase(objParaConverter){
-//   objParaConverter
-// }
+
+// Utilizando a estrutura do Switch faça um programa que simule uma calculadora básicaO programa recebe
+// como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores
+// numéricos na ordem que foram inseridos. Por exemplo: calculadora (2, ‘+’, 3). A função efetuará a soma de 2 e
+// 3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. Crie um caso default para operações inválidas.
+
+function calculadora(operador1, operando, operador2) {
+  switch (operando) {
+    case "+":
+      console.log(operador1 + operador2);
+      break;
+    case "-":
+      console.log(operador1 - operador2);
+      break;
+    case "*":
+      console.log(operador1 * operador2);
+      break;
+    case "/":
+      console.log(operador1 / operador2);
+      break;
+    case "%":
+      console.log(operador1 % operador2);
+      break;
+    default:
+      console.log("Insira um operando; +, -, *, / ou %");
+  }
+}
+
+calculadora(1, "+", 5);
