255012df0feb4f05776ce324f4b2b159b650d52b
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-20 23:54:43 -0300

    obj

--- Javascript/orientacaoAObj.js
+++ Javascript/orientacaoAObj.js
@@ -1,1 +1,12 @@
 //Introdução a OO em JS.
+
+//1 Objeto é uma coleção dinamica de pares CHAVE: VALOR
+
+const produto = new Object();
+
+//object é uma função, ao usar uma função com o new, obtemos uma função construtora que instancia um objeto.
+//Podemos atribuir um novo objeto chave:valor com notação ponto:
+produto.nome = "Cadeira";
+
+//e também como um array, sendo o que está dentro do array a chave
+produto["Marca do produto"] = "Generica";
