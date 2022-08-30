d20a2baa19d2f4bba19b2b59a81461cbf991bb9b
ijefin <jeffersonmecmiler@hotmail.com>
2022-08-30 14:25:14 -0300

    class

--- Javascript/classe1.js
+++ Javascript/classe1.js
@@ -1,3 +1,10 @@
 class Lancamento {
-    constructor(nome= 'Generico', valor = 0)
+  constructor(nome = "Generico", valor = 0) {
+    this.nome = nome;
+    this.valor = valor;
+  }
 }
+
+//classes em JS também são funções.
+console.log(typeof Lancamento);
+
