e0f6114297dba6344c2905587a33534403ff0744
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-02 17:12:16 -0300

    map

--- Javascript/arrayJs.js/map.js
+++ Javascript/arrayJs.js/map.js
@@ -1,1 +1,9 @@
 //Map realiza uma transformação em de um array em outro.
+
+// note que o array original não sofre alteração
+const nums = [1, 2, 3, 4, 5];
+
+const vezes2 = nums.map((c) => c * 2);
+console.log(vezes2, nums);
+
+
