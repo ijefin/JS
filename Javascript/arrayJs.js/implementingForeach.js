ac4f5ba5146a4c6e09f8a3f056def4c503265eec
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-01 21:09:00 -0300

    implementing the foreach!

---
+++ Javascript/arrayJs.js/implementingForeach.js
@@ -0,0 +1,10 @@
+//foreach por baixo dos panos
+Array.prototype.forEach2 = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+const aprovados = ["Jeff", "Lucas", "Gomes"];
+
+aprovados.forEach2((nome, indice) => console.log(nome, indice + 1));
