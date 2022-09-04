0cf8db203dab94f719613c47f423b347898dc0e7
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-04 14:49:36 -0300

    bora

--- Javascript/arrayJs.js/map.js
+++ Javascript/arrayJs.js/map.js
@@ -1,4 +1,4 @@
-//Map realiza uma transformação em de um array em outro.
+//Map realiza uma transformação em de um array em outro, diferente do foreach, que simplesmente percorre um array e devolve os dados
 
 // note que o array original não sofre alteração
 const nums = [1, 2, 3, 4, 5];
@@ -6,9 +6,11 @@ const nums = [1, 2, 3, 4, 5];
 const vezes2 = nums.map((c) => c * 2);
 console.log(vezes2, nums);
 
-const soma10 = (e) => e + 10;
-
-console.log(soma10(20));
-
 //O método que passamos para um map, deve possuir um retorno explicito.
-//Lets implement the map function!
+const otherNums = [1, 2, 3, 4, 5];
+const soma10 = (e) => e + 10;
+const triplo = (e) => e * 3;
+const paraReal = (e) => `R$ ${parseFloat(e).toFixed(2).replace(".", ",")}`;
+
+resultado = otherNums.map(soma10).map(triplo).map(paraReal);
+console.log(resultado);
