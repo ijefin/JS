4b0809171de278831368a91bc08021c0136879f1
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-05 14:51:38 -0300

    filter function

---
+++ Javascript/arrayJs.js/filter.js
@@ -0,0 +1,11 @@
+const produtos = [
+  { nome: "Notebook", preco: "5995", fragil: true },
+  { nome: "Teclado Gamer Hyperx Allow Fps RGB", preco: 450.45, fragil: true },
+  { nome: "Copo De Vidro", preco: "7.49", fragil: true },
+  { nome: "Copo de Plástico", preco: "1.99", fragil: false },
+  { nome: "Tampo de aço", preco: "359.89", fragil: false },
+  { nome: "Monitor Gamer Mancer ", preco: "1845.90", fragil: false },
+];
+
+const filtroFalso = produtos.filter((c) => c.fragil === false);
+console.log(filtroFalso);
---
+++ Javascript/arrayJs.js/implementingMap.js
@@ -0,0 +1,9 @@
+//that's how the map function works.
+
+Array.prototype.map2 = function (callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i], i, this));
+  }
+  return newArray;
+};
