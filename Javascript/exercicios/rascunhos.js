8521798554a02aecd0e65ba9d6c0b4e0d64c0c77
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-16 11:18:28 -0300

    some training

---
+++ Javascript/exercicios/rascunhos.js
@@ -0,0 +1,72 @@
+const horaChegada = (hora) =>
+  console.log(
+    `Se tu vens por exemplo, às ${hora} da tarde, desde às ${
+      hora - 1
+    } eu começarei a ser feliz!`
+  );
+
+horaChegada(3);
+
+function solucao1(ano) {
+  if (ano < -4000) {
+    while (ano > -4000) {
+      console.log(ano--);
+    }
+  }
+
+  ano >= -4000 && ano <= 476
+    ? console.log("ANTIGA")
+    : ano >= 477 && ano <= 1453
+    ? console.log("MEDIA")
+    : ano >= 1454 && ano <= 1789
+    ? console.log("MEDIA")
+    : console.log("CONTEMPORANEA");
+}
+
+solucao1(-7000);
+
+// ANTIGA se o ano está entre -4000 (4000 a.C.) e 476;
+// MEDIA se o ano está entre 477 e 1453;
+// MODERNA se o ano está entre 1454 e 1789;
+// CONTEMPORANEA se o ano está após 1789.
+
+function solucao(media, frequencia, projetoFinal) {
+  const freqMin = 75;
+  const mediaMin = 60;
+  const minProjeto = 85;
+
+  if (
+    (media >= mediaMin && frequencia >= freqMin && projetoFinal > 0) ||
+    (media <= mediaMin && frequencia >= freqMin && projetoFinal >= minProjeto)
+  ) {
+    console.log("APROVADO");
+  } else {
+    console.log("REPROVADO");
+  }
+}
+
+solucao(80, 74, 78);
+
+function purrinha(jogador1, jogador2, qtdTampinha) {
+  if (
+    jogador1 === jogador2 ||
+    jogador1 + jogador2 > 6 ||
+    jogador1 + jogador2 < 0
+  ) {
+    console.log("PALPITE INVALIDO");
+  } else if (jogador1 === qtdTampinha) {
+    console.log("JOGADOR 1 GANHOU");
+  } else if (jogador2 === qtdTampinha) {
+    console.log("JOGADOR 2 GANHOU");
+  } else if (jogador2 === qtdTampinha && jogador1 === qtdTampinha) {
+    console.log("Empate");
+  }
+}
+
+purrinha(3, 3, 0);
+
+// PALPITE INVALIDO caso tenha dois pedidos iguais;
+// ABSURDO caso algum jogador tenha feito um palpite maior que o maior número possível de tampinhas em jogo, que seriam 6 (3 de cada jogador);
+// JOGADOR 1 GANHOU se o jogador 1 tiver ganhado;
+// JOGADOR 2 GANHOU se o jogador 2 tiver ganhado;
+// EMPATE se tiver ocorrido empate.
