4a2e8b0f78a9c8ebc3fb80ed9ba38a337e537574
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-18 00:48:03 -0300

    js

--- Javascript/exercicios/saude.js
+++ Javascript/exercicios/saude.js
@@ -8,11 +8,16 @@ function pressaoAlta(array) {
     (pacientesB) => pacientesB >= 25 && pacientesB < 29
   );
   const dadosClasseC = array.filter((pacientesC) => pacientesC > 29);
-  
-  console.log(dadosClasseA.length, dadosClasseB.length, dadosClasseC.length);
+
+  const quantidade =
+    dadosClasseA.length * 0.16 +
+    dadosClasseB.length * 0.31 +
+    dadosClasseC.length * 0.59;
+
+  return console.log(quantidade);
 }
 
-pressaoAlta([12, 25, 36, 32, 14, 24]);
+pressaoAlta([12, 35, 23, 10, 21, 27, 28]);
 
 // Aproximadamente 16% dos pacientes com IMC menor que 25 têm pressão alta;
 // Aproximadamente 31% dos pacientes com IMC entre 25 e 29 têm pressão alta;
