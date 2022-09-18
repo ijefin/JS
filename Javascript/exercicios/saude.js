1d3605bb1fd035a5080a6554e5429e48ea6a3fd0
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-18 00:07:08 -0300

    array pressao alta

--- Javascript/exercicios/saude.js
+++ Javascript/exercicios/saude.js
@@ -1,1 +1,5 @@
 // O Índice de Massa Corporal, conhecido como IMC, é um importante indicador utilizado por médicos e nutricionistas para analisar o estado de pacientes.
+
+function pressaoAlta(array) {
+    
+}
