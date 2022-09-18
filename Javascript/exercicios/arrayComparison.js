ddd66082b6582196d02259a9565c5a7489169d5f
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-18 18:47:08 -0300

    go

--- Javascript/exercicios/arrayComparison.js
+++ Javascript/exercicios/arrayComparison.js
@@ -2,10 +2,13 @@ function compareTriplets(a, b) {
   let maxValue = 2;
 
   for (let i = 0; i <= maxValue; i++) {
-    const arrayA = [2, 8, 1];
-    const arrayB = [4, 6, 7];
+    const arrayA = [5, 8, 1];
+    const arrayB = [5, 6, 7];
 
-    console.log(arrayA[i] > arrayB[i]);
+    const teste = [arrayA[i] > arrayB[i]];
+
+    teste.filter((item) => item.true);
+    console.log(teste);
   }
 }
 
