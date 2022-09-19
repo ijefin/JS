e0b95e78eb589620b62703bf5359f08b0d689542
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-19 11:45:27 -0300

    array comparison

--- Javascript/exercicios/arrayComparison.js
+++ Javascript/exercicios/arrayComparison.js
@@ -1,17 +1,21 @@
 function compareTriplets(a, b) {
-  let maxValue = 2;
+  let zezin = 0;
+  let outroZezin = 0;
+  let finalArray = [];
 
-  for (let i = 0; i <= maxValue; i++) {
-    const arrayA = [5, 8, 1];
-    const arrayB = [5, 6, 7];
-
-    const teste = [arrayA[i] > arrayB[i]];
-
-    teste.filter((item) => item.true);
-    console.log(teste);
+  for (let i = 0; i <= 2; i++) {
+    if (a[i] > b[i]) {
+      zezin += 1;
+    } else if (b[i] > a[i]) {
+      outroZezin += 1;
+    }
   }
+
+  finalArray.push(zezin, outroZezin);
+  return console.log(finalArray);
 }
 
-compareTriplets();
+compareTriplets([5, 6, 7], [3, 6, 10]);
+compareTriplets([17, 28, 30], [99, 16, 8]);
 
 //Compare if the value is bigger than or equals.
