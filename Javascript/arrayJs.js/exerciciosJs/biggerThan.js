be7fbd9a03fa0874da2dcc9f9cbd3c7c4b0f9016
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-23 19:50:38 -0300

    upgrade

--- Javascript/arrayJs.js/exerciciosJs/biggerThan.js
+++ Javascript/arrayJs.js/exerciciosJs/biggerThan.js
@@ -1,6 +1,5 @@
 function biggerThan(numb1, numb2) {
-  if (typeof primeiro != typeof segundo) return false;
+  if (typeof numb1 != "number" || typeof numb2 != "number") return false;
   return console.log(numb1 >= numb2);
 }
-
-biggerThan(1254, 12);
+biggerThan(2, 5);
