d287e83c980e00c8eb9398103c0042dc62b1e849
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-24 22:54:06 -0300

    lets repeeeeeeeeat

--- Javascript/arrayJs/exerciciosJs/someRepeat.js
+++ Javascript/arrayJs/exerciciosJs/someRepeat.js
@@ -9,3 +9,7 @@ function letsRepeat(el, repeat) {
   console.log(`There is ${repeat} elements named ${el} `);
 }
 letsRepeat(5, 25);
+
+// function repetir(item, quantidade) {
+//   return Array(quantidade).fill(item);
+// }
