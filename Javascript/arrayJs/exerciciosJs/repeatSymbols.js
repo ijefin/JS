5312b8387101ef8c7fe0f8c525de2a81d5df8064
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-24 22:53:27 -0300

    go

--- Javascript/arrayJs/exerciciosJs/repeatSymbols.js
+++ Javascript/arrayJs/exerciciosJs/repeatSymbols.js
@@ -1,7 +1,17 @@
 function repeatSymbols(amount) {
-  for (let i = "+"; i <= amount; i++) {
-    console.log(i);
+  let result = "";
+  for (let i = 1; i <= amount; i++) {
+    result += "+";
   }
+  console.log(result);
+  return console.log(`there is ${result.length} symbols of PLUS`);
 }
 
 repeatSymbols(12);
+
+//filling with array.fill
+// function simboloMais(quantidade) {
+//   return console.log(Array(quantidade).fill("+").join(""));
+// }
+
+// simboloMais(25);
