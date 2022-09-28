1548bd5e87d4b0430819952ce4e945bde39359ba
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-28 15:07:55 -0300

    count

--- Javascript/frontend/NotificationPage/index.js
+++ Javascript/frontend/NotificationPage/index.js
@@ -3,9 +3,18 @@ const allNotificationCard =
 console.log(allNotificationCard);
 
 function markAllAsRead() {
+  let allMarkedAsRead;
   for (i = 0; i < 7; i++) {
     allNotificationCard[i].style.backgroundColor = "white";
   }
+  allMarkedAsRead = true;
+  if (markAllAsRead) {
+    document.getElementById("#notification-number").innerHTML = 0;
+  }
+}
+
+function markSelectedAsRead() {
+  allNotificationCard.style.backgroundColor = "white";
 }
 
 function anotherFunction() {
