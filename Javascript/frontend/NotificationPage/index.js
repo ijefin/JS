66a644b72755912052fc32d20e6cbbf78361f2a6
ijefin <jeffersonmecmiler@hotmail.com>
2022-09-29 12:34:39 -0300

    go

--- Javascript/frontend/NotificationPage/index.js
+++ Javascript/frontend/NotificationPage/index.js
@@ -3,24 +3,9 @@ const allNotificationCard =
 console.log(allNotificationCard);
 
 function markAllAsRead() {
-  let allMarkedAsRead;
   for (i = 0; i < 7; i++) {
     allNotificationCard[i].style.backgroundColor = "white";
   }
-  allMarkedAsRead = true;
-  if (markAllAsRead) {
-    document.getElementById("#notification-number").innerHTML = 0;
-  }
 }
 
-function markSelectedAsRead() {
-  allNotificationCard.style.backgroundColor = "white";
-}
 
-function anotherFunction() {
-  var allRead = false;
-  if (allNotificationCard[i].style.backgroundColor === "white") {
-    allRead = true;
-  }
-  console.log(allRead);
-}
