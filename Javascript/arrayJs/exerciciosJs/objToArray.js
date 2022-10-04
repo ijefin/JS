546cea37081550bf6c6ef6eff009829707ceb1d1
ijefin <jeffersonmecmiler@hotmail.com>
2022-10-03 22:11:56 -0300

    refactory

--- Javascript/arrayJs/exerciciosJs/objToArray.js
+++ Javascript/arrayJs/exerciciosJs/objToArray.js
@@ -1,9 +1,7 @@
 //object.entries retorna um array com os elementos chave e valor que correspondem ao atributo de um obj
 function objToArray(obj) {
-  const finalResult = Object.entries(obj);
-  return console.log(finalResult);
+  return console.log(Object.entries(obj));
 }
 
 objToArray({ nome: "Jeff", idade: 23, profissao: "Programador" });
 // result [ [ 'nome', 'Jeff' ], [ 'idade', 23 ], [ 'profissao', 'Programador' ] ]
-    
